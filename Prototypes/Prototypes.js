7bce1b9b8070be5477641311a064f0fd6cc9956e
DForshner <DForshner@gmail.com>
2013-08-03 23:52:53 -0400

    Added ProtoType Example

---
+++ Prototypes/Prototypes.js
@@ -0,0 +1,41 @@
+﻿// Javascript doesn't have classes in the tranditional sense.
+// Javascript uses prototype inheritance.
+// Each object has a special prototype property
+// (__proto__) that gets inherited from the parent
+function Dog() {
+};
+
+// Add the bark function to the Dog prototype.
+// This could be done inside the Dog constructor but then a new bark
+// method would be created for every dog.  By declaring it on the
+// prototype one instance of brak is created and shared by all dogs.
+Dog.prototype.bark = function(){
+    return "woof!";
+};
+
+// We can set a constructors prototype to be another object.
+function SmallDog() {
+    Dog.call(this);
+};
+SmallDog.prototype = new Dog();
+SmallDog.prototype.constructor = SmallDog;
+
+// Each constructor has an associated prototype object.
+function BigDog() {
+    Dog.call(this);
+};
+BigDog.prototype = new Dog();
+BigDog.prototype.constructor = BigDog;
+
+// An object can override the property of its parent
+BigDog.prototype.bark = function(){
+    return "WOOF!";
+};
+
+function FancySmallDog() {
+};
+FancySmallDog.prototype = new SmallDog();
+FancySmallDog.prototype.constructor = FancySmallDog;
+FancySmallDog.prototype.yip = function() {
+    return "Yip!";
+};
---
+++ Prototypes/Prototypes.tests.js
@@ -0,0 +1,51 @@
+﻿// An object shares the prototype of the parent object.
+test("A dog is a dog", 1, function() {
+    var dog = new Dog();
+  deepEqual(dog.__proto__, Dog.prototype);
+});
+
+test("Dogs bark", 1, function() {
+  // When a constructor creates an object its parent is set to the
+  // prototype object associated with the constructor that created it.
+  var dog = new Dog();
+  equal(dog.bark(), 'woof!');
+});
+
+test("Small dogs bark", 1, function() {
+  var dog = new SmallDog();
+  equal(dog.bark(), 'woof!');
+});
+
+test("Small dogs are dogs", 2, function() {
+  var dog = new SmallDog();
+  equal(dog.__proto__, SmallDog.prototype);
+  ok(dog instanceof Dog);
+});
+
+test("Big dogs BARK!", 1, function() {
+  var dog = new BigDog();
+  equal(dog.bark(), 'WOOF!');
+});
+
+test("Fancy small dogs are dogs", 2, function() {
+  var dog = new FancySmallDog();
+  equal(dog.__proto__, FancySmallDog.prototype);
+  ok(dog instanceof Dog);
+});
+
+test("Fancy small dogs can bark and yip", 2, function() {
+  var dog = new FancySmallDog();
+  ok('bark' in dog);
+  ok('yip' in dog);
+});
+
+// hasOwnProperty tests that the object has the property not
+// the prototype
+test("Annoying fancy small dogs Yap", 2, function() {
+  var dog = new FancySmallDog();
+  dog.yap = function() {
+    return "Yap Yap Yap";
+  };
+  ok(dog.hasOwnProperty('yap'));
+  ok(!dog.hasOwnProperty('yip'));
+});
