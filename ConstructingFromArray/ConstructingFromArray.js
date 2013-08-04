61d0bf8574c58094dfb753d91771aa9fb5fe6c17
DForshner <DForshner@gmail.com>
2013-08-03 23:52:38 -0400

    Added Constructing From Array Example

---
+++ ConstructingFromArray/ConstructingFromArray.js
@@ -0,0 +1,26 @@
+﻿function Person(name, children) {
+  this.name = name;
+  this.children = children;
+};
+
+// Create a new method for the function.prototype
+// that can be used instead of the new statement.
+// Based on: http://stackoverflow.com/questions/813383/how-can-i-construct-an-object-using-an-array-of-values-for-parameters-rather-th#answer-813401
+Function.prototype.new = function() {
+  // Create a fake constructor that will apply
+  // the real constructor as a method.
+  var args = arguments;
+  var constructor = this;
+  function Fake(){
+    constructor.apply(this, args)
+  };
+  // Set the fake prototype to the original constructor.
+  Fake.prototype = constructor.prototype;
+  // Create an object using the fake constructor and return it. 
+  return new Fake;
+};
+
+// Helper method 
+Function.prototype.applyNew = function() {
+  return this.new.apply(this, arguments[0]);
+};
---
+++ ConstructingFromArray/ConstructingFromArray.tests.js
@@ -0,0 +1,25 @@
+﻿test("A person can have children", 1, function() {
+  var children = [new Person('Ben'), new Person('Dan')];
+  var agnes = new Person("Agnes", children);
+  equal(agnes.children.length, 2);
+});
+
+test("A person can be created from an array", 1, function() {
+  var children = [new Person('Ben'), new Person('Dan')];
+  var args = [ ['Bob'], children ];
+  var bob = Person.new.apply(Person, args);
+  equal(bob.children.length, 2);
+});
+
+test("A person can be created from an array 2.0", 1, function() {
+  var children = [new Person('Ben'), new Person('Dan')];
+  var args = [ ['Bob'], children ];
+  var bob = Person.applyNew(args);
+  equal(bob.children.length, 2);
+});
+
+test("A person can be created from an array 2.0 with no kids", 1, function() {
+  var args = ['Bob'];
+  var bob = Person.applyNew(args);
+  equal(bob.children, undefined);
+});
